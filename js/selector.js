c4916a675672a351e095a76f9112f54254ca28d0
Mskkhan <68050061+Mskkhan@users.noreply.github.com>
2020-09-06 11:30:10 +0530

    Add files via upload

---
+++ js/selector.js
@@ -0,0 +1,12 @@
+console.log("this my javascript");
+/*
+element selector
+single element selector
+multiple element selector
+*/
+// single element selector
+// element = document.getElementById('myFirst');
+//element = element.className;
+//element = element.childNodes;
+element = document.querySelector("#myFirst");
+console.log(element);
